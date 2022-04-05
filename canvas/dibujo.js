45de7c81a02e1e9a4435f4af63a87154ff354208
De-la-torre <salomencarlos@gmail.com>
2022-04-04 22:37:29 -0500

    melo

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,27 +1,24 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-var lineas = 30;
+var lineas = 50;
 var l = 0;
-var yi, xf;
-
-while(l < lineas)
-{
-    yi = 10 * l;
-    xf = 10 * (l + 1);
-    dibujarLinea("black", 0, yi, xf, 300);
-    console.log("Linea " + l);
+var yi, xf, y2;
+while(l < lineas){
+    y2 = 500-(10*l+10);
+    yi = 10*l;
+    xf = 10*l+10
+    dibujarLienzo("black", 0, yi, xf, 500);
+    dibujarLienzo("black", yi, 0, 500, xf);
+    dibujarLienzo("black", 500, y2, xf, 500);
+    dibujarLienzo("black", y2, 0, 0, xf);
     l = l + 1;
 }
 
-dibujarLinea("black", 1, 1, 1, 300);
-dibujarLinea("black", 1, 299, 299, 299);
-
-function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
-{
+function dibujarLienzo(color, xinicial, yinicial, xlinea, ylinea){
     lienzo.beginPath();
-    lienzo.strokeStyle = color;
+    lienzo.strokeStyle = (color)
     lienzo.moveTo(xinicial, yinicial);
-    lienzo.lineTo(xfinal, yfinal);
+    lienzo.lineTo(xlinea, ylinea);
     lienzo.stroke();
     lienzo.closePath();
 }

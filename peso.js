572191f57f62ce3be05fa2aedb1a1fd659f36d5d
De-la-torre <salomencarlos@gmail.com>
2022-04-04 19:48:27 -0500

    ajustes

--- peso.js
+++ peso.js
@@ -1,5 +1,5 @@
-var usuario = "cual es tu peso"
-var planeta1 = marte
+var usuario = prompt("¿cual es tu peso?");
+var planeta = parseInt(prompt("Elige tu planeta\n1 es marte, 2 es jupiter"))
 var peso = parseInt(usuario)
 var g_tierra = 9.8;
 var g_marte = 3.7;
@@ -7,12 +7,7 @@ var g_jupiter = 24.8;
 var peso_final;
 var nombre;
 
-const result = (peso, planeta) => {
-    let planeta = peso * g_marte / g_tierra;
-    return resultado
-}
-function calcularpeso() {
-if(planeta == marte)
+if(planeta == 1)
 {
     peso_final = peso * g_marte / g_tierra;
     nombre = "marte"
@@ -24,7 +19,6 @@ if(planeta == marte)
 else{
     peso_final = 1000000;
     nombre = "el planeta que imaginaste"
-}
 }
 peso_final = parseInt(peso_final)
 document.write("Tu peso en " + nombre +  + peso_final + " kilos");

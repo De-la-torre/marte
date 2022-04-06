879943b8616353b64a9e70feb6ff8a97c32779b8
De-la-torre <salomencarlos@gmail.com>
2022-04-05 22:33:50 -0500

    algo melo

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,18 +1,11 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 35;
-var l = 0;
-var yi, xf, y2;
-while(l < lineas){
-    y2 =350-(10*l+10);
-    yi = 10*l;
-    xf = 10*l+10
-    dibujarLienzo("black", 0, yi, xf, 350);
-    dibujarLienzo("black", yi, 0, 350, xf);
-    dibujarLienzo("black", 350, y2, xf, 350);
-    dibujarLienzo("black", y2, 0, 0, xf);
-    l = l + 1;
-}
+
 
 function dibujarLienzo(color, xinicial, yinicial, xlinea, ylinea){
     lienzo.beginPath();
@@ -21,4 +14,24 @@ function dibujarLienzo(color, xinicial, yinicial, xlinea, ylinea){
     lienzo.lineTo(xlinea, ylinea);
     lienzo.stroke();
     lienzo.closePath();
+}
+
+function dibujoPorClick()
+{
+    var lineas = parseInt(texto.value);;
+var l = 0;
+var yi, xf, y2, x2;
+var espacio = ancho / lineas;
+
+while(l < lineas){
+    x2 = 350+(10*l+10);
+    y2 =350-(10*l+10);
+    yi = espacio *l;
+    xf = espacio *l+10
+    dibujarLienzo("black", 0, yi, xf, 350);
+    dibujarLienzo("black", yi, 0, 350, xf);
+    dibujarLienzo("black", 350, y2, xf, 350);
+    dibujarLienzo("black", y2, 0, 0, xf);
+    l = l + 1;
+}
 }
---
+++ teclas/teclas.js
@@ -0,0 +1,55 @@
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39
+};
+
+console.log(teclas);
+
+document.addEventListener("keyup", dibujarTeclado)
+
+var cuadrito = document.getElementById("area")
+var papel = cuadrito.getContext("2d")
+var x = 150;
+var y = 150;
+
+dibujarLienzo("red", 149, 149, 151, 151, papel);
+
+function dibujarLienzo(color, xinicial, yinicial, xlinea, ylinea, lienzo){
+    lienzo.beginPath();
+    lienzo.strokeStyle = (color)
+    lienzo.lineWidth = 3;
+    lienzo.moveTo(xinicial, yinicial);
+    lienzo.lineTo(xlinea, ylinea);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+function dibujarTeclado(evento)
+{
+    var colorcito = "black";
+    var movimiento = 10;
+    switch(evento.keyCode)
+    {
+        case teclas.UP:
+        dibujarLienzo(colorcito, x, y, x, y - movimiento, papel);
+        y = y -movimiento;
+        break;
+        case teclas.DOWN:
+            dibujarLienzo(colorcito, x, y, x, y + movimiento, papel);
+            y = y + movimiento;
+        break;
+        case teclas.LEFT:
+            dibujarLienzo(colorcito, x, y, x - movimiento, y, papel);
+            x = x - movimiento;
+        break;
+        case teclas.RIGHT:
+            dibujarLienzo(colorcito, x, y, x + movimiento, y, papel);
+            x = x  + movimiento;
+        break;
+        default:
+            console.log("otra tecla");
+            break;
+    }
+}

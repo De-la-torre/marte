7666a6d2645f070c894a929871562aae8108fbb2
De-la-torre <salomencarlos@gmail.com>
2022-04-05 19:32:20 -0500

    no joda

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,15 +1,15 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-var lineas = 40;
+var lineas = 35;
 var l = 0;
 var yi, xf, y2;
 while(l < lineas){
-    y2 =400-(10*l+10);
+    y2 =350-(10*l+10);
     yi = 10*l;
     xf = 10*l+10
-    dibujarLienzo("black", 0, yi, xf, 400);
-    dibujarLienzo("black", yi, 0, 400, xf);
-    dibujarLienzo("black", 400, y2, xf, 400);
+    dibujarLienzo("black", 0, yi, xf, 350);
+    dibujarLienzo("black", yi, 0, 350, xf);
+    dibujarLienzo("black", 350, y2, xf, 350);
     dibujarLienzo("black", y2, 0, 0, xf);
     l = l + 1;
 }
